f756b72382db228bd5f1d1bad147609e86fbd595
Dayree <aysetubakahraman293@gmail.com>
2022-05-12 21:25:52 +0300

    formeklendi

---
+++ blokzinciri/Blok.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blokzinciri
+{
+    public class Blok
+    {
+        public int Indeks { get; set; }
+        public DateTime IslemZamani { get; set; }
+        public string OncekiSifreleme { get; set; }
+        public string Sifreleme { get; set; }
+        public string Veri { get; set; }
+
+        public Blok(DateTime islemZamani, string oncekiSifreleme, string data)
+        {
+            Indeks = 0;
+            IslemZamani = islemZamani;
+            OncekiSifreleme = oncekiSifreleme;
+            Veri = data;
+            Sifreleme = HesaplaSifreleme();
+        }
+
+        public string HesaplaSifreleme()
+        {
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{IslemZamani}-{OncekiSifreleme ?? ""}-{Veri}");
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+    }
+}
---
+++ blokzinciriekrani/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using blokzinciri;
+using Newtonsoft.Json;
+
+namespace blokzinciriekrani
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Blokzincir twobCoin = new Blokzincir();
+            twobCoin.EkleBlok(new Blok(DateTime.Now, null, "{sender:Hüseyin,receiver:Tuba,amount:10}"));
+            twobCoin.EkleBlok(new Blok(DateTime.Now, null, "{sender:Tuba,receiver:Hüseyin,amount:5}"));
+            twobCoin.EkleBlok(new Blok(DateTime.Now, null, "{sender:Tuba,receiver:Hüseyin,amount:5}"));
+
+            Console.WriteLine(JsonConvert.SerializeObject(twobCoin, Formatting.Indented));
+
+            Console.WriteLine(JsonConvert.SerializeObject(twobCoin.Zincir[1], Formatting.Indented));
+            label1.Text = JsonConvert.SerializeObject(twobCoin, Formatting.Indented);
+        }
+
+    }
+}
