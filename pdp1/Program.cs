9560337ff646f78d5338f89eb7ae3dd993f74d1b
Dayree <aysetubakahraman293@gmail.com>
2022-05-12 21:08:32 +0300

    Blokzinciri kullanıldı.

--- pdp1/Program.cs
+++ pdp1/Program.cs
@@ -11,7 +11,16 @@ namespace pdp1
     {
         static void Main(string[] args)
         {
-            
+            Blokzincir twobCoin = new Blokzincir();
+            twobCoin.EkleBlok(new Blok(DateTime.Now, null, "{sender:Hüseyin,receiver:Tuba,amount:10}"));
+            twobCoin.EkleBlok(new Blok(DateTime.Now, null, "{sender:Tuba,receiver:Hüseyin,amount:5}"));
+            twobCoin.EkleBlok(new Blok(DateTime.Now, null, "{sender:Tuba,receiver:Hüseyin,amount:5}"));
+
+            Console.WriteLine(JsonConvert.SerializeObject(twobCoin, Formatting.Indented));
+
+            Console.WriteLine(JsonConvert.SerializeObject(twobCoin.Zincir[1], Formatting.Indented));
+
+            Console.Read();
         }
     }
 }
