2f8f2a2b4742d01f6e4691cb1702a96544113d9b
Dayree <aysetubakahraman293@gmail.com>
2022-05-12 20:51:34 +0300

    Add project files.

---
+++ pdp1/Blokzinciri.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace pdp1
+{
+    public class Blokzincir
+    {
+        public IList<Blok> Zincir { set; get; }
+
+        public Blokzincir()
+        {
+            BaslangicZinciri();
+            EkleBlok();
+        }
+
+
+        public void BaslangicZinciri()
+        {
+            Zincir = new List<Blok>();
+        }
+
+        public Blok BloguOlustur()
+        {
+            return new Blok(DateTime.Now, null, "{}");
+        }
+
+        public void EkleBlok()
+        {
+            Zincir.Add(BloguOlustur());
+        }
+
+        public Blok SonZinciriGetir()
+        {
+            return Zincir[Zincir.Count - 1];
+        }
+
+        public void EkleBlok(Blok block)
+        {
+            Blok sonZincir = SonZinciriGetir();
+            block.Indeks = sonZincir.Indeks + 1;
+            block.OncekiSifreleme = sonZincir.Sifreleme;
+            block.Sifreleme = block.HesaplaSifreleme();
+            Zincir.Add(block);
+        }
+
+        public bool GecerliMi()
+        {
+            for (int i = 1; i < Zincir.Count; i++)
+            {
+                Blok simdikiBlok = Zincir[i];
+                Blok öncekiBlok = Zincir[i - 1];
+
+                if (simdikiBlok.Sifreleme != simdikiBlok.HesaplaSifreleme())
+                {
+                    return false;
+                }
+
+                if (simdikiBlok.OncekiSifreleme != öncekiBlok.Sifreleme)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
